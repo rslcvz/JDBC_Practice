af2b3918dbe6b7c1dd7abd5cf526e14a53cc5c81
rslcvz <rslceviz@gmail.com>
2020-01-23 15:58:36 -0500

    Assertions

--- src/test/java/RestApiTests/SpartanTestsWithParameters.java
+++ src/test/java/RestApiTests/SpartanTestsWithParameters.java
@@ -1,9 +1,11 @@
 package RestApiTests;
 import static io.restassured.RestAssured.*;
 import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertNotNull;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import javafx.scene.layout.Priority;
 import org.testng.Assert;
 import org.testng.Assert.*;
 import org.testng.annotations.BeforeClass;
@@ -13,20 +15,28 @@ import org.testng.annotations.Test;
 public class SpartanTestsWithParameters {
 
 @BeforeClass
-    public static void setUp(){
+    public static void setUp() {
 
     // Base URL   http://3.83.88.105:8000/api
     //BASE URI means all API end point share this portion.
-    //We dont have to write everything
-    RestAssured.baseURI = "http://3.83.88.105:1000/ords/hr/regions/6";
-    //3.83.88.105:1000/ords/hr/regions/6
+    //We dont have to write everything  -- You can give more specific
+    RestAssured.baseURI = "http://54.164.29.19";
+    RestAssured.port = 8000;
+    RestAssured.basePath = "/api";
+
+}
+    @Test
+            public void test(){
+        Response response = get("/hello");
+        System.out.println("response.statusCode() = " + response.statusCode());
+        System.out.println(response.asString());
+        System.out.println(response.getHeader("content-type"));
+
 }
 @Test
     public void helloTest(){
-            Response response = get("http://54.164.29.19:8000/api/hello");
-    System.out.println("response.statusCode() = " + response.statusCode());
-    System.out.println(response.asString());
-    System.out.println(response.getHeader("content-type"));
+
+    Response response = get("/hello");
     assertEquals(200, response.statusCode());
     assertEquals("Hello from Sparta", response.asString());
     // rest assured ;ibrary provide multiple method with same meaning
@@ -36,7 +46,21 @@ public class SpartanTestsWithParameters {
 
 
 }
-//@Test
-//    public void
+@Test
+    public void Hello_EndPoint_HeaderContains_Test(){
+
+    Response response = get("/hello");
+    String headerValue = response.getHeader("Date");
+    System.out.println("headerValue = " + headerValue);
+    //its giving you the date or null(Both Passing)
+    //if its null dont pass, this is how we do it.
+    assertNotNull(headerValue);
+
+    assert response.getHeaders().hasHeaderWithName("Date");
+    // Get header returns String, you cannt assert with a number.
+    assertEquals ("17", response.getHeader("content-length"));
+
+}
+
 
 }
