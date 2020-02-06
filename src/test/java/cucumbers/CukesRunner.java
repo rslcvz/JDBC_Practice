6c89119b42a72d228af54955ed8ae86fb886d0c9
rslcvz <rslceviz@gmail.com>
2020-02-06 15:44:01 -0500

    New Changes 3

---
+++ src/test/java/cucumbers/CukesRunner.java
@@ -0,0 +1,14 @@
+package cucumbers;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features"
+, glue = "step_definitions"
+, dryRun = true)
+public class CukesRunner {
+
+
+}
---
+++ target/test-classes/features/getSingleSpartan.feature
@@ -0,0 +1,6 @@
+
+Feature: User Get Spartan Data
+  Scenario : Authenticated User Should be able to Get Spartan Data
+    Given Credentials with USER_role is provided
+    When user try to send get request on "/spartans/10"
+    Then user should get status code 200
