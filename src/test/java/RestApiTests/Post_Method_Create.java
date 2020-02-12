f884fdbc75e907eb31ed2d44c7c9c9af3f0ea69b
rslcvz <rslceviz@gmail.com>
2020-02-12 12:26:10 -0500

    Last Update 2/12/2020

--- src/test/java/RestApiTests/Post_Method_Create.java
+++ src/test/java/RestApiTests/Post_Method_Create.java
@@ -22,9 +22,9 @@ public class Post_Method_Create {
     public void Add_NewSpartan_Test() {
 
         Map<String, Object> bodyMap = new HashMap<String, Object>();
-        bodyMap.put("id", 22);
-        bodyMap.put("name", "Yasin");
-        bodyMap.put("gender", "Male");
+        bodyMap.put("id", 486);
+        bodyMap.put("name", "Myra");
+        bodyMap.put("gender", "Female");
         bodyMap.put("phone", "12189113345");
 
         given()
@@ -36,7 +36,7 @@ public class Post_Method_Create {
                 .statusCode(201)
                 .contentType(ContentType.JSON)
                 .body("success", is("A Spartan is Born!"))
-                .body("data.name", equalToIgnoringCase("Yasin"))
+                .body("data.name", equalToIgnoringCase("Myra"))
                 .body("data.phone", hasToString("12189113345"));   // 201  Created
 
     }
